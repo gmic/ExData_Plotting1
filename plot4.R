be29459f5cf684c62de832235225d2c8eb6e2f60
Gerhard Messelink <gerhard.messelink@gmail.com>
2017-08-10 08:20:58 +0200

    plot4 added

--- plot4.R
+++ plot4.R
@@ -19,11 +19,21 @@ hpc_subset <- hpc[as.Date(hpc$Date, "%d/%m/%Y") >= startDate & as.Date(hpc$Date,
 
 hpc_subset$DateTime <- as.POSIXct(strptime(paste(hpc_subset$Date, hpc_subset$Time), "%d/%m/%Y %H:%M:%S"))
 
-plot(hpc_subset$DateTime, hpc_subset$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
-lines(hpc_subset$DateTime, hpc_subset$Sub_metering_2, type="l", col="red")
-lines(hpc_subset$DateTime, hpc_subset$Sub_metering_3, type="l", col="blue")
+# par(mfrow = c(2, 2), mar = c(4, 4, 2, 1), oma = c(0, 0, 2, 0))
+par(mfrow = c(2, 2))
 
-legend("topright", lwd=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+with(hpc_subset, {
+  plot(DateTime, Global_active_power, type="l", xlab="", ylab="Global Active Power")
 
-dev.copy(png, file="plot3.png")
+  plot(DateTime, Voltage, type="l", xlab="datetime", ylab="Voltage")
+
+  plot(DateTime, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+  lines(DateTime, Sub_metering_2, type="l", col="red")
+  lines(DateTime, Sub_metering_3, type="l", col="blue")
+  legend("top", bty="n", xjust=1, lwd=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  
+  plot(DateTime, Global_reactive_power, type="l", xlab="datetime", ylab="Global Reactive Power")
+})
+
+dev.copy(png, file="plot4.png")
 dev.off()
